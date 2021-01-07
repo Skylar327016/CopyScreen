a663edb04565add3623cf002fbeb77a7041920c2
Skylar <apa6143171@gmail.com>
2021-01-07 13:05:54 +0000

    Initial Commit

---
+++ CopyScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CopyScreen
+//
+//  Created by 陳家豪 on 2021/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
